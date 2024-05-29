44d789f2945fffb6569eeebf232cfeffe613521c
Andrea Sgura <andre-56@hotmail.it>
2024-05-29 17:28:59 +0200

    error messages in italian

--- app/Http/Requests/StoreComicRequest.php
+++ app/Http/Requests/StoreComicRequest.php
@@ -24,10 +24,25 @@ class StoreComicRequest extends FormRequest
     public function rules()
     {
         return [
-            'thumb' => 'required|max:255',
+            'thumb' => 'required|max:255|min:3',
             'price' => 'required|max:20',
-            'series'=> 'required|max:50',
-            'type'=> 'required|max:50'
+            'series' => 'required|max:50',
+            'type' => 'required|max:50'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'thumb.required' => 'Il campo immagine è obbligatorio',
+            'thumb.min' => 'Il campo immagine deve contenere almeno :min caratteri',
+            'thumb.max' => 'Il campo immagine deve contenere al massimo :max caratteri',
+            'price.required' => 'Il campo prezzo è obbligatorio',
+            'price.max' => 'Il campo prezzo deve contenere al massimo :max caratteri',
+            'series.required' => 'Il campo serie è obbligatorio',
+            'series.max' => 'Il campo serie deve contenere al massimo :max caratteri',
+            'type.required' => 'Il campo tipo è obbligatorio',
+            'type.max' => 'Il campo tipo deve contenere al massimo :max caratteri',
         ];
     }
 }
--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -19,7 +19,7 @@
         @csrf
         <div class="mb-3">
             <label for="thumb" class="form-label">Inserisci url immagine</label>
-            <input type="text" class="form-control @error('thumb') is-invalid @enderror " id="thumb" aria-describedby="thumbHelp" name="thumb" >
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror " id="thumb" aria-describedby="thumbHelp" name="thumb" value="{{ old('thumb') }}" >
             <div id="titleHelp" class="form-text">bhooo</div>
             @error('thumb')
             <div class="alert alert-danger">{{ $message }}</div>
@@ -27,21 +27,21 @@
           </div>
           <div class="mb-3">
             <label for="price" class="form-label">Prerzzo</label>
-            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"  name="price" >
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"  name="price" value="{{ old('price') }}" >
             @error('price')
             <div class="alert alert-danger">{{ $message }}</div>
             @enderror
           </div>
           <div class="mb-3">
             <label for="series" class="form-label ">Nome serie</label>
-            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series"  name="series" >
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series"  name="series" value="{{ old('series') }}" >
           @error('series')
           <div class="alert alert-danger">{{ $message }}</div>
           @enderror
           </div>
           <div class="mb-3">
             <label for="type" class="form-label ">Tipo</label>
-            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type"  name="type" >
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type"  name="type" value="{{ old('type') }}" >
           @error('type')
           <div class="alert alert-danger">{{ $message }}</div>
           @enderror
