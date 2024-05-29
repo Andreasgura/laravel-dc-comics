15c84a94c1973a57fb30c4503f55dfd723d512a0
Andrea Sgura <andre-56@hotmail.it>
2024-05-29 17:10:02 +0200

    error message on each input tag

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,6 +5,7 @@
 @section('content')
 <section class="container">
     <h1>Crea</h1>
+    <!-- unique dive for all errors
     @if ($errors->any())
     <div class="alert alert-danger">
         <ul>
@@ -13,25 +14,37 @@
             @endforeach
         </ul>
     </div>
-    @endif
+    @endif -->
     <form action="{{route('comics.store')}}" method="POST">
         @csrf
         <div class="mb-3">
             <label for="thumb" class="form-label">Inserisci url immagine</label>
-            <input type="text" class="form-control" id="thumb" aria-describedby="thumbHelp" name="thumb" >
+            <input type="text" class="form-control @error('thumb') is-invalid @enderror " id="thumb" aria-describedby="thumbHelp" name="thumb" >
             <div id="titleHelp" class="form-text">bhooo</div>
+            @error('thumb')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
           </div>
           <div class="mb-3">
             <label for="price" class="form-label">Prerzzo</label>
-            <input type="text" class="form-control" id="price"  name="price" >
+            <input type="text" class="form-control @error('price') is-invalid @enderror" id="price"  name="price" >
+            @error('price')
+            <div class="alert alert-danger">{{ $message }}</div>
+            @enderror
           </div>
           <div class="mb-3">
-            <label for="series" class="form-label">Nome serie</label>
-            <input type="text" class="form-control" id="series"  name="series" >
+            <label for="series" class="form-label ">Nome serie</label>
+            <input type="text" class="form-control @error('series') is-invalid @enderror" id="series"  name="series" >
+          @error('series')
+          <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
           </div>
           <div class="mb-3">
-            <label for="type" class="form-label">Tipo</label>
-            <input type="text" class="form-control" id="type"  name="type" >
+            <label for="type" class="form-label ">Tipo</label>
+            <input type="text" class="form-control @error('type') is-invalid @enderror" id="type"  name="type" >
+          @error('type')
+          <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
           </div>
         <button type="submit" class="btn btn-primary">Crea</button>
         <button type="reset" class="btn btn-danger">Annulla</button>
